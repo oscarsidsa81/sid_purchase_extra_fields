3d66da1f448f5778017ebc5d66c89f860547bfcd
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-26 16:53:12 +0100

    reviso manifest

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     "version": "15.0.1.0.0",
     "category": "Purchases",
     "summary": "Modulo paraguas para compatibilidad. Agrupa dependencias funcionales de compras.",
-    "author": "SIDSA / Custom",
+    "author": "oscarsidsa81",
     "license": "LGPL-3",
     "depends": [
         "sid_purchase_core",
